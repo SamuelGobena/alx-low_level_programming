7bde5b9efaa967a386cb5fc0e0ebc3b8387018b1
SamuelGobena <samuelead47@gmail.com>
2022-10-13 19:49:23 +0000

    task 7 Intel syntax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("Size of a char: %lu byte\n", sizeof(charType));
-	printf("Size of a int: %lu byte(s)\n", sizeof(intType));
-	printf("Size of a double: %lu byte(s)\n", sizeof(longType));
-	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %i byte\n", sizeof(charType));
+	printf("Size of a int: %i byte(s)\n", sizeof(intType));
+	printf("Size of a double: %i byte(s)\n", sizeof(longType));
+	printf("Size of a float: %i byte(s)\n", sizeof(floatType));
 
 	return (0);
 }

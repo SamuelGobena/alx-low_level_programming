87c762b3d58b3c97a9648fe118e0d8bb05cfb0ed
SamuelGobena <samuelead47@gmail.com>
2022-10-21 08:38:49 +0000

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks for digit (0 through 9)
+ * print_numbers - checks for digit (0 through 9)
  *
  * Return: Always 0;
  */
 void print_numbers(void)
 {
 	int e;
+
 	for (e = 48; e < 58; e++)
 	{
 		if (e != 50)
 		{
 			if (e != 52)
 			{
-				_putchar(e);
+				putchar(e);
 			}
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }

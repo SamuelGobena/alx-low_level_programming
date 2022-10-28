9525dec7efe12d56e4ee2232f01237b49605c689
SamuelGobena <samuelead47@gmail.com>
2022-10-28 08:28:41 +0000

    Task 3 correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int flag = 1;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			flag = 1;
-			break;
-		}
-		if (flag == 1)
-		{
 			return (s1[i] - s2[i]);
 			i++;
 		}
 	}
-
-	return (s1[i] - s2[i]);
+	return (0);
 }

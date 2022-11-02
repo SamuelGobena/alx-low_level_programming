91264e1ed05bf7b4c5dbf3fe8c0b8c085e1b7464
SamuelGobena <samuelead47@gmail.com>
2022-11-02 22:52:05 +0000

    correction power of

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y.
- * @x: integers
- * @y: power of y.
- * Returns: success 1
+ * _pow_recursion- eturns the factorial of a given number.
+ * @x: input
+ * @y: power
+ * Return: Succes 0
  */
 int _pow_recursion(int x, int y)
 {
-	if(y < 0)
+	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);

764bf3cf2f3806cf534b3a52e9d26887ff5c4885
SamuelGobena <samuelead47@gmail.com>
2022-11-08 17:32:59 +0000

    creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,6 +5,7 @@
 /**
  * *create_array - a function that creates an array of chars
  * @c: char type
+ * @size: sizeof char
  * Return: a pointer to the array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
@@ -14,16 +15,16 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (0);
-	
-	ptr = (char*)malloc(size * sizeof(char));
-	
+
+	ptr = (char *)malloc(size * sizeof(char));
+
 	if (ptr == NULL)
 		return (0);
-	
+
 	for (i = 0; i < size; i++)
 		*(ptr + i) = c;
-	
+
 	*(ptr + i) = '\0';
-	
+
 	return (ptr);
 }

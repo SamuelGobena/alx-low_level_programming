eacfe92c9d82f7b25ac38af1d887ceffeceace25
SamuelGobena <samuelead47@gmail.com>
2022-11-15 10:38:53 +0000

    corrections structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,13 @@ dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *str)
 {
 	int len = 0;
-	
+
 	while (*str++)
 		len++;
-	
+
 	return (len);
 }
 /**
- *
  * _strcopy - Copies a string pointed to by src, including the
  * terminating null byte, to a buffer pointed to by dest.
  * @dest: The buffer storing the string copy.
@@ -30,7 +29,7 @@ int _strlen(char *str)
 char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
-	
+
 	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
 	dest[index] = '\0';
@@ -47,21 +46,21 @@ char *_strcopy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
-	
+
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	
+
 	doggo = malloc(sizeof(dog_t));
 	if (doggo == NULL)
 		return (NULL);
-	
+
 	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (doggo->name == NULL)
 	{
 		free(doggo);
 		return (NULL);
 	}
-	
+
 	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (doggo->owner == NULL)
 	{
@@ -69,10 +68,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(doggo);
 		return (NULL);
 	}
-	
+
 	doggo->name = _strcopy(doggo->name, name);
 	doggo->age = age;
 	doggo->owner = _strcopy(doggo->owner, owner);
-	
+
 	return (doggo);
 }

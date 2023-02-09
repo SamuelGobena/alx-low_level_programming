f4a413dadcc41777e3f9ea93e02fa1c006a2d717
samuelgobena <samuelead47@gmail.com>
2023-02-09 18:58:13 +0000

    a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,12 +4,14 @@
  * set_bit - sets the value of a bit at a given index to 1.
  * @n: A pointer to the bit.
  * @index: The index to set the value at indices start at 0.
- * Return: -1 or 1.
+ * Return:1 or 1.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 0))
 		return (-1);
+
 	*n ^= (1 << index);
+
 	return (1);
 }
